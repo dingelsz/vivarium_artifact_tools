e8b2ef6843e327c97b07e6c8fedd637c7cbd413d
Zach Dingels <>
2018-08-30 15:41:45 -0700

    created test file moved add_population and reduce_draws inside of ArtifactTool as private methods

--- artifact_tool.py
+++ artifact_tool.py
@@ -4,93 +4,6 @@ import numpy as np
 from functools import lru_cache
 
 
-def reduce_draws(table, val_col="value"):
-    """Creates a DataFrame with mean and CI values obtained across draws.
-
-    Parameters
-    ----------
-    table:
-        A pandas DataFrame that contains a "draw" column
-    col_name:
-        The name of the column inside table that the mean and CI values should
-        be computed from. The column should contain numeric values.
-
-    Returns
-    -------
-    A table that summarizes key statistical values for a specific column
-    """
-
-    assert "draw" in table.columns, "Table does not have a column named draw"
-
-    drawless_table = table.query('draw == 0')
-
-    # create identifiers for each row, independent of draws
-    columns = drawless_table.columns.tolist()
-    columns = [c for c in columns if c not in ['draw', val_col]]
-
-    # turn the identifiers into strings
-    identifiers = []
-    for index, row in drawless_table.sort_values(by=columns).iterrows():
-        row_list = row[columns].tolist()
-        ID = '-'.join([str(row) for row in row_list])
-        identifiers.append(ID)
-
-    # create a table that has draws as rows and identifiers as columns
-    table = table.sort_values(by=['draw'] + columns)
-    values = table[val_col].values
-    values = values.reshape(len(table) // len(identifiers), len(identifiers))
-    value_df = pd.DataFrame(values, columns=identifiers)
-
-    # remove the values column from our drawless table and add columns for the
-    # stats we want
-    result_df = drawless_table[columns].sort_values(by=columns)
-    result_df[val_col + "_mean"] = [value_df[col].values.mean() for col in identifiers]
-    result_df['lower 2.5'] = [np.percentile(value_df[col].values, 2.5) for col in identifiers]
-    result_df['upper 97.5'] = [np.percentile(value_df[col].values, 97.5) for col in identifiers]
-    result_df = result_df.reset_index(drop=True)
-
-    return result_df
-
-
-def add_population(table, pop_table):
-    """ Maps data on age, sex and year to populations.
-
-    Parameters
-    ----------
-    table:
-        A pandas DataFrame that contains "age", "sex" and "year" columns
-    pop_table:
-        A pandas DataFrame that contains "age", "sex", "year" and "population"
-        columns
-
-    Returns
-    -------
-    table with a column named population appended to it.
-    """
-    assert all([col_name in table.columns for col_name in ["age", "year", "sex"]]), "table does not have all the required columns"
-
-    assert all([col_name in pop_table.columns for col_name in ["age", "year", "sex", "population"]]), "table does not have all the required columns"
-
-    # Set each column to a str so we can hash them
-    table_age = table.age.astype(str)
-    table_year = table.year.astype(str)
-    table_sex = table.sex.astype(str)
-    pop_age = pop_table.age.astype(str)
-    pop_year = pop_table.year.astype(str)
-    pop_sex = pop_table.sex.astype(str)
-
-    table_hash = (table_age + table_year + table_sex)
-    pop_table['hash'] = (pop_age + pop_year + pop_sex)
-
-    @lru_cache(maxsize=256)
-    def hash_to_pop(hash_key):
-        return float(pop_table[pop_table.hash == hash_key].population.values)
-    table['population'] = table_hash.map(hash_to_pop)
-
-    pop_table = pop_table.drop(columns=['hash'])
-    return table
-
-
 class ArtifactTool():
     # TODO Write a query parser that checks if query is valid for table
 
@@ -162,9 +75,8 @@ class ArtifactTool():
         table = table[table.age <= upper]
         table = table[table.age >= lower]
 
-        table = reduce_draws(table)
-        pop_table = self._hdf.get('/population/structure')
-        table = add_population(table, pop_table)
+        table = self._reduce_draws(table)
+        table = self._add_population(table)
         table['exposed'] = table.average * table.population
 
         exposure_table = table.groupby(['parameter']).aggregate(np.sum)
@@ -178,10 +90,10 @@ class ArtifactTool():
         table = table[table.age <= upper]
         table = table[table.age >= lower]
 
-        table = reduce_draws(table)
+        table = self._reduce_draws(table)
         # TODO Remove population is we don't need it
         pop_table = self._hdf.get('/population/structure')
-        table = add_population(table, pop_table)
+        table = self._add_population(table)
         table = table.sort_values(by=['cause', 'parameter'])
 
         weighted_risk = table.population * table.value_mean
@@ -197,3 +109,84 @@ class ArtifactTool():
         results['risk'] = pd.Series([risk_factor for _ in range(len(results))])
         results['relative_risk'] = pd.Series(risks)
         return results
+
+    def _reduce_draws(self, table, val_col="value"):
+        """Creates a DataFrame with mean and CI values obtained across draws.
+
+        Parameters
+        ----------
+        table:
+            A pandas DataFrame that contains a "draw" column
+        col_name:
+            The name of the column inside table that the mean and CI values should
+            be computed from. The column should contain numeric values.
+
+        Returns
+        -------
+        A table that summarizes key statistical values for a specific column
+        """
+
+        assert "draw" in table.columns, "Table does not have a column named draw"
+
+        drawless_table = table.query('draw == 0')
+
+        # create identifiers for each row, independent of draws
+        columns = drawless_table.columns.tolist()
+        columns = [c for c in columns if c not in ['draw', val_col]]
+
+        # turn the identifiers into strings
+        identifiers = []
+        for index, row in drawless_table.sort_values(by=columns).iterrows():
+            row_list = row[columns].tolist()
+            ID = '-'.join([str(row) for row in row_list])
+            identifiers.append(ID)
+
+        # create a table that has draws as rows and identifiers as columns
+        table = table.sort_values(by=['draw'] + columns)
+        values = table[val_col].values
+        values = values.reshape(len(table) // len(identifiers), len(identifiers))
+        value_df = pd.DataFrame(values, columns=identifiers)
+
+        # remove the values column from our drawless table and add columns for the
+        # stats we want
+        result_df = drawless_table[columns].sort_values(by=columns)
+        result_df[val_col + "_mean"] = [value_df[col].values.mean() for col in identifiers]
+        result_df['lower 2.5'] = [np.percentile(value_df[col].values, 2.5) for col in identifiers]
+        result_df['upper 97.5'] = [np.percentile(value_df[col].values, 97.5) for col in identifiers]
+        result_df = result_df.reset_index(drop=True)
+
+        return result_df
+
+
+    def _add_population(self, table):
+        """ Maps data on age, sex and year to populations.
+
+        Parameters
+        ----------
+        table:
+            A pandas DataFrame that contains "age", "sex" and "year" columns
+
+        Returns
+        -------
+        table with a column named population appended to it.
+        """
+        assert all([col_name in table.columns for col_name in ["age", "year", "sex"]]), "table does not have all the required columns"
+        pop_table = self._hdf.get('/population/structure')
+        # Set each column to a str so we can hash them
+        table_age = table.age.astype(str)
+        table_year = table.year.astype(str)
+        table_sex = table.sex.astype(str)
+        pop_age = pop_table.age.astype(str)
+        pop_year = pop_table.year.astype(str)
+        pop_sex = pop_table.sex.astype(str)
+
+        table_hash = (table_age + table_year + table_sex)
+        pop_table['hash'] = (pop_age + pop_year + pop_sex)
+
+        @lru_cache(maxsize=256)
+        def hash_to_pop(hash_key):
+            return float(pop_table[pop_table.hash == hash_key].population.values)
+        table['population'] = table_hash.map(hash_to_pop)
+
+        pop_table = pop_table.drop(columns=['hash'])
+        return table
